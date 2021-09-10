09eb1df2db21a3a5dd8cbe89fd9e88730994719e
Rick Blalock <rblalock@gmail.com>
2021-09-09 20:23:26 -0400

    add to build script

---
+++ data/db.js
@@ -0,0 +1,24 @@
+import Database from 'better-sqlite3';
+import path from 'path';
+
+let db;
+
+try {
+	const dir = path.resolve(process.cwd(), 'data', 'vercel_test');
+	db = new Database(dir, {
+		readonly: true,
+		fileMustExist: true,
+	});
+} catch (err) {
+	console.error(err);
+	console.error('In path: ', dir);
+}
+
+export const sampleQuery = () => {
+	try {
+		const results = db.prepare('SELECT * FROM sample').all();
+		return results;
+	} catch (err) {
+		return err;
+	}
+};
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,27 +1,4 @@
-import Database from 'better-sqlite3';
-import path from 'path';
-
-let db;
-
-try {
-	const dir = path.resolve(process.cwd(), 'data', 'vercel_test');
-	db = new Database(dir, {
-		readonly: true,
-		fileMustExist: true,
-	});
-} catch (err) {
-	console.error(err);
-	console.error('In path: ', dir);
-}
-
-const sampleQuery = () => {
-	try {
-		const results = db.prepare('SELECT * FROM sample').all();
-		return results;
-	} catch (err) {
-		return err;
-	}
-};
+import { sampleQuery } from '../../data/db'
 
 export default async (req, res) => {
 	try {
